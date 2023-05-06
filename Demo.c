2e0f03e83e9980fd9b493a1c917c0683a73c0826
AFFAN SIRAJUDDIN KHAN <132735435+affankhan21@users.noreply.github.com>
2023-05-06 13:34:22 +0530

    Add files via upload

---
+++ Demo.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+
+
+void Display()
+{
+
+printf("hello world !!!\n");
+}
+int main()
+{
+    printf("inside main\n");
+Display();
+printf("outside main\n");
+    return 0;
+}
---
+++ array34.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+
+
+    int Arr[5];
+    printf("%d\n",Arr[4]);
+}
