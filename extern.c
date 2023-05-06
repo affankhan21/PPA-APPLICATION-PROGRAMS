e56f078fe74c153b58a871a360588b9848d579f8
AFFAN SIRAJUDDIN KHAN <132735435+affankhan21@users.noreply.github.com>
2023-05-06 13:36:35 +0530

    Add files via upload

---
+++ extern.c
@@ -0,0 +1,3 @@
+#include<stdio.h>
+int B=34;
+int C;
---
+++ externdemo1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int A=120;
+extern int B;
+extern int C;
+int main()
+{
+    printf("the value of A is %d\n",A);
+    printf("the value of B is %d\n",B);
+    printf("the value of C is %d\n",C);
+
+
+
+
+
+
+
+
+    return 0;
+}
